f98aa435f56574bf4eebfd4779e8654865670c40
Martin Synák <martin.synak123@gmail.com>
2024-06-04 00:16:59 +0200

    Handling change on inputs, storing it in state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleSubmit(event){
+    event.preventDefault();
+  }
+
+  function handleChange(event){
+    const { name, value } = event.target;
+
+    setValues({
+      ...values,
+      [name]: value
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={handleChange} value={values.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={handleChange} value={values.password}/>
         </div>
       </div>
 
