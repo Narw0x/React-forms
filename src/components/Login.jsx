a0bb191c99c22a6d347a7c2df4be1046d79975e8
Martin Synák <martin.synak123@gmail.com>
2024-06-11 00:30:26 +0200

    validating values in form on keystroke with focus validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,16 +6,35 @@ export default function Login() {
     password: ''
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  });
+
+  const emailIsInvalid = didEdit.email && !values.email.includes('@');
+
   function handleSubmit(event){
     event.preventDefault();
   }
 
   function handleChange(event){
     const { name, value } = event.target;
+    setDidEdit({
+      ...didEdit,
+      [name]: false
+    });
 
     setValues({
       ...values,
       [name]: value
+    });
+  }
+
+  function handleBlur(event){
+    const { name } = event.target;
+    setDidEdit({
+      ...didEdit,
+      [name]: true
     });
   }
 
@@ -26,7 +45,10 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={handleChange} value={values.email}/>
+          <input id="email" type="email" name="email" onBlur={handleBlur} onChange={handleChange} value={values.email}/>
+          <div className="control-error">
+            {emailIsInvalid && 'Please enter a valid email'}
+          </div>
         </div>
 
         <div className="control no-margin">
